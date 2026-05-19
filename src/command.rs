72df78d982029267de590b453bea784d2813e5d0
Jordi <55429631+jorbush@users.noreply.github.com>
2026-05-19 16:12:39 +0200

    codecrafters submit [skip ci]

--- src/command.rs
+++ src/command.rs
@@ -1,18 +1,21 @@
 #[derive(Debug, PartialEq)]
 pub enum Command {
+    Echo,
     Exit,
 }
 
 impl Command {
     pub fn from_str(s: &str) -> Option<Command> {
         match s {
+            "echo" => Some(Command::Echo),
             "exit" => Some(Command::Exit),
             _ => None,
         }
     }
 
-    pub fn execute(&self) -> () {
+    pub fn execute(&self, params: &str) -> () {
         match self {
+            Command::Echo => println!("{}", params),
             Command::Exit => std::process::exit(0),
         }
     }
@@ -26,5 +29,12 @@ mod tests {
     fn test_from_str() {
         assert_eq!(Command::from_str("exit"), Some(Command::Exit));
         assert_eq!(Command::from_str("invalid"), None);
+    }
+
+    #[test]
+    fn test_execute_echo() {
+        let command = Command::Echo;
+        let params = "Hello, world!";
+        command.execute(params);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -17,9 +17,13 @@ fn main() {
             continue;
         }
 
-        match Command::from_str(input) {
+        let (command, params) = match input.split_once(' ') {
+            Some((command, params)) => (command, params),
+            None => (input, ""),
+        };
+        match Command::from_str(command) {
             Some(command) => {
-                command.execute();
+                command.execute(params);
                 continue;
             }
             None => println!("{}: command not found", input),
